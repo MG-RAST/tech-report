6ba3b7dfab2852cd971cb898f4d4a77bf843de0b
Folker Meyer <folker@anl.gov>
2013-06-19 10:46:25 -0500

    rebuild repo after snafu

---
+++ mg-rast-tech-report/labels.pl
@@ -0,0 +1,277 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:mgrast-job-sizes/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:collections-finished/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:project-page-editing-buttons/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:workbench-download/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mgrast-v3-pipeline/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:quad-chart-analysis-example/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:table-group-by-class/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:barchart-from-workbench/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:collections-filter-by-biome/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:nucleotide-with-contamination/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:rarefaction/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chapter:downloads/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:rank-abundance/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:project-spreadsheet/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:subsystems-functions-piechart/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:identifier/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Data-selection-expanded/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mgrast_analysis-schema/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:source-hits-distribution/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:table-organism-abundance/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Collections-how-to-save/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:data-model/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:analysis-page/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mgrastv2VSv3/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page-table/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:blat-alignment/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:collections-twin/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page-tree-options/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:metagenome-overview/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:boxplots/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:mgrastemail/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:metadata/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page-radial-tree/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Inbox-File-Information/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:GSC-MIxS-checklist-information/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page-tree-additional-bar-charts/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:nucleotide-hist-amplicon/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:alpha-diversity/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:heatmap/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:workbench/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cost_per_megabase/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:linkin.cgi/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:project-page/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:collections-total-list1/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:browse-for-metagenomes/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Inbox/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:nucleotide-hist-ok/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:project-spreadsheet-with-3-samples/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:collections-filtered-but-too-many/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sitemap/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:source-hits-distribution/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page-tree-order-level-with-phylum-colors/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sharing/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:nucleotide-hist-barcodes/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page-bar-chart/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:analysis-page-selecting-lean-and-obese/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:classification-pie-chart/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/DRISEEDETAIL/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/secton:Quality/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:browse-window/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:Data-sharing-in-mg-rast/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:hit-types/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:using-collections/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:project-spreadsheet-multiple-tabs/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:metagenome-browser/;
+$external_labels{$key} = "$URL/" . q|mg-rast-tech-report.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
